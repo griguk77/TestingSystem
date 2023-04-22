45402aaa68473fbaf694c7009b78ffa12d77ff52
griguk77 <griguk77@gmail.com>
2023-04-23 02:10:40 +0300

    Добавление дизайна экрана результата тестирования

--- app/src/main/java/ru/studyguk/testingsystem/presentation/QuestionFragment.kt
+++ app/src/main/java/ru/studyguk/testingsystem/presentation/QuestionFragment.kt
@@ -1,10 +1,11 @@
-package ru.studyguk.testingsystem
+package ru.studyguk.testingsystem.presentation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ru.studyguk.testingsystem.R
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
