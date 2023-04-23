099b92456f642eb55e89a015f23c6e30e8dcf681
griguk77 <griguk77@gmail.com>
2023-04-23 17:08:54 +0300

    Добавление навигации между экранами

--- app/src/main/java/ru/studyguk/testingsystem/presentation/LoginFragment.kt
+++ app/src/main/java/ru/studyguk/testingsystem/presentation/LoginFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import ru.studyguk.testingsystem.R
+import ru.studyguk.testingsystem.databinding.FragmentLoginBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class LoginFragment : Fragment() {
+    private lateinit var binding : FragmentLoginBinding
     private var param1: String? = null
     private var param2: String? = null
 
@@ -22,7 +25,14 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+        binding.textViewRegistrLink.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registrFragment)
+        }
+        binding.buttonLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_catalogFragment)
+        }
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/ru/studyguk/testingsystem/presentation/RegistrFragment.kt
+++ app/src/main/java/ru/studyguk/testingsystem/presentation/RegistrFragment.kt
@@ -5,13 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import ru.studyguk.testingsystem.R
+import ru.studyguk.testingsystem.databinding.FragmentRegistrBinding
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class RegistrFragment : Fragment() {
-
+    private lateinit var binding: FragmentRegistrBinding
     private var param1: String? = null
     private var param2: String? = null
 
@@ -23,8 +25,11 @@ class RegistrFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registr, container, false)
+        binding = FragmentRegistrBinding.inflate(layoutInflater)
+        binding.buttonLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_registrFragment_to_loginFragment)
+        }
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
