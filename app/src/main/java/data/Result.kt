97a87b76a68922d517544a33440d43138caa7f38
griguk77 <griguk77@gmail.com>
2023-04-23 23:32:05 +0300

    Добавление data-класса результата

---
+++ app/src/main/java/data/Result.kt
@@ -0,0 +1,20 @@
+package data
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
+import androidx.room.PrimaryKey
+
+@Entity(foreignKeys = [
+    ForeignKey(entity = Test::class,
+        parentColumns = ["id"],
+        childColumns = ["idTest"],
+        onDelete = CASCADE)])
+data class Result(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val idTest: Int,
+    val user: String,
+    val points: Int,
+    val date: String
+)
