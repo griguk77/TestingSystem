fd1a80c7d0948d3e29d1a8515a9896937c637502
griguk77 <griguk77@gmail.com>
2023-04-23 22:50:36 +0300

    Добавление data-класса текста результата

--- app/src/main/java/data/Test.kt
+++ app/src/main/java/data/Test.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class Subject (
+data class Test (
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val name: String
